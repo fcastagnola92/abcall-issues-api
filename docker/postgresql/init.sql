6e7a70a1ce5578af3dae9abfe1a04a3df056db5d
migueltovar <loganitsolutions@outlook.com>
2024-10-12 17:59:49 -0500

    creating database objects 🚀

--- docker/postgresql/init.sql
+++ docker/postgresql/init.sql
@@ -8,3 +8,36 @@ BEGIN
    END IF;
 END
 $do$;
+
+
+
+CREATE TABLE IF NOT EXISTS issue_state(
+   id UUID PRIMARY KEY,
+   name VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS issue(
+   id UUID PRIMARY KEY,
+   auth_user_id UUID,
+   auth_user_agent_id UUID,
+   status UUID,
+   subject VARCHAR(255),
+   description TEXT,
+   created_at TIMESTAMP WITH TIME ZONE,
+   closed_at TIMESTAMP WITH TIME ZONE,
+   channel_plan_id UUID,
+   CONSTRAINT fk_status
+        FOREIGN KEY (status) 
+        REFERENCES issue_state (id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS issue_attachment (
+    id UUID PRIMARY KEY,
+    file_path VARCHAR(255),
+    issue_id UUID,
+    CONSTRAINT fk_issue
+        FOREIGN KEY (issue_id) 
+        REFERENCES issue (id)
+        ON DELETE CASCADE
+);
