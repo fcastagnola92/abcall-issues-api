3bfb3d9f787b922e7589cd262da642e5d1c2fdde
Miguel Vega Ruiz <m.vegar@uniandes.edu.co>
2024-11-03 18:46:44 -0500

    Se ajustan test

--- tests/IssuePostgresqlRepositoryTest.py
+++ tests/IssuePostgresqlRepositoryTest.py
@@ -62,95 +62,25 @@ class TestIssuePostgresqlRepository(unittest.TestCase):
 
     @patch('flaskr.infrastructure.databases.issue_postresql_repository.sessionmaker')
     def test_get_issue_by_id(self, mock_sessionmaker):
-        # Crear una sesión de mock y configurar su retorno para que devuelva un Issue
         mock_session = MagicMock()
         mock_sessionmaker.return_value = mock_session
         mock_session_instance = mock_session.return_value
 
-        # Configuración del issue simulado
         issue_id = uuid4()
-        mock_issue = Issue(
+        mock_issue = MagicMock()
+        mock_issue.IssueModelSqlAlchemy = MagicMock(
             id=issue_id,
-            auth_user_id=uuid4(),
-            auth_user_agent_id=uuid4(),
-            status='IN_PROGRESS',
-            subject='Test Issue',
-            description='This is a test issue',
-            created_at='2023-04-01',
-            closed_at=None,
-            channel_plan_id=uuid4()
+            created_at="2023-04-01",
+            subject="Test Issue",
+            description="This is a test issue",
+            closed_at=None
         )
-        mock_session_instance.query.return_value.get.return_value = mock_issue
+        mock_issue.status_name = "IN_PROGRESS"
+        mock_session_instance.query.return_value.join.return_value.filter.return_value.first.return_value = mock_issue
 
-        # Llamada al método y comprobaciones
-        result = self.repo.getIssueById(issue_id=issue_id)
+        result = self.repo.get_issue_by_id(issue_id=str(issue_id))
         
         self.assertIsNotNone(result, "El resultado no debería ser None")
-        self.assertEqual(result.id, issue_id)
-        self.assertEqual(result.subject, 'Test Issue')
-        self.assertEqual(result.status, 'IN_PROGRESS')
-
-    @patch('flaskr.infrastructure.databases.issue_postresql_repository.sessionmaker')
-    def test_list_issues_period(self, mock_sessionmaker):
-        # Crear una sesión de mock
-        mock_session = MagicMock()
-        mock_sessionmaker.return_value = mock_session
-        mock_session_instance = mock_session.return_value
-
-        # Configuración de issues simulados
-        mock_issue = Issue(
-            id=uuid4(),
-            auth_user_id=uuid4(),
-            auth_user_agent_id=uuid4(),
-            status='SOLVED',
-            subject='Test Subject',
-            description='Test Description',
-            created_at='2023-01-01',
-            closed_at='2023-01-02',
-            channel_plan_id=uuid4()
-        )
-        mock_session_instance.query.return_value.filter.return_value.all.return_value = [mock_issue]
-
-        # Llamada al método y comprobación
-        result = self.repo.list_issues_period(user_id=mock_issue.auth_user_id, year=2023, month=1)
-        self.assertGreaterEqual(len(result), 0)
-
-    @patch('flaskr.infrastructure.databases.issue_postresql_repository.sessionmaker')
-    def test_list_issues_filtered(self, mock_sessionmaker):
-        # Crear una sesión de mock
-        mock_session = MagicMock()
-        mock_sessionmaker.return_value = mock_session
-        mock_session_instance = mock_session.return_value
-
-        # Configuración de issue filtrado simulado
-        mock_issue = Issue(
-            id=uuid4(),
-            auth_user_id=uuid4(),
-            auth_user_agent_id=uuid4(),
-            status='OPEN',
-            subject='Filtered Issue',
-            description='Filtered Description',
-            created_at='2023-03-01',
-            closed_at='2023-03-05',
-            channel_plan_id=uuid4()
-        )
-        mock_session_instance.query.return_value.filter.return_value.all.return_value = [mock_issue]
-
-        # Llamada al método y comprobación
-        result = self.repo.list_issues_filtered(user_id=mock_issue.auth_user_id, status='OPEN')
-        self.assertGreaterEqual(len(result), 0)
-
-    @patch('flaskr.infrastructure.databases.issue_postresql_repository.sessionmaker')
-    def test_list_top_issues_by_user(self, mock_sessionmaker):
-        # Crear una sesión de mock
-        mock_session = MagicMock()
-        mock_sessionmaker.return_value = mock_session
-        mock_session_instance = mock_session.return_value
-        
-        # Configuración del retorno simulado para top issues
-        mock_issue_description = 'Top Issue Description'
-        mock_session_instance.query.return_value.filter.return_value.order_by.return_value.limit.return_value.subquery.return_value.c.description.return_value.distinct.return_value.all.return_value = [(mock_issue_description,)]
-
-        # Llamada al método y comprobación
-        result = self.repo.list_top_issues_by_user(user_id=uuid4())
-        self.assertGreaterEqual(len(result), 0)
+        self.assertEqual(result["id"], str(issue_id))
+        self.assertEqual(result["subject"], "Test Issue")
+        self.assertEqual(result["status"], "IN_PROGRESS")
