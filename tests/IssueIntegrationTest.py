cd4577b51f0d6659ebb72744048678645cb73973
fabioCastagnola <f.castagnola@uniandes.edu.co>
2024-11-04 01:10:38 -0500

    Update IssueIntegrationTest.py

--- tests/IssueIntegrationTest.py
+++ tests/IssueIntegrationTest.py
@@ -16,20 +16,6 @@ class IssueIntegrationTest(unittest.TestCase):
         
 
 
-    
-    def test_should_return_not_found_when_the_get_path_does_not_exist(self):
-        data = {
-            'auth_user_id': fake.uuid4(),
-            'auth_user_agent_id': fake.uuid4(),
-            'subject': fake.word(),
-            'description': fake.sentence()
-        }
-        error_message = "Action not found"
-
-        response = self.client.get('/issue/getFakePath', content_type='multipart/form-data', data=data)
-
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
-        self.assertEqual(response.json["message"], error_message)
 
 
 
